71528f9a4d62a5bd97ca07b6f2eda358dd03ba84
Sachin Nimshang <sachinnimshan@gmail.com>
2020-06-09 08:48:10 +0530

    Updates

---
+++ precache-manifest.bd8b2f9da6ea76329529ab1e93f2ae52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d3dca981cc3f4406e45b59dd4c1e85",
+    "url": "/React-To-Do-Sachin-Nimshan/index.html"
+  },
+  {
+    "revision": "10073fee19fb8d8be819",
+    "url": "/React-To-Do-Sachin-Nimshan/static/css/main.826e2a03.chunk.css"
+  },
+  {
+    "revision": "2932d8ad92168289ab30",
+    "url": "/React-To-Do-Sachin-Nimshan/static/js/2.a1bb2094.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-To-Do-Sachin-Nimshan/static/js/2.a1bb2094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10073fee19fb8d8be819",
+    "url": "/React-To-Do-Sachin-Nimshan/static/js/main.acfa0286.chunk.js"
+  },
+  {
+    "revision": "28c062f0eb80e42b07aa",
+    "url": "/React-To-Do-Sachin-Nimshan/static/js/runtime-main.9635a6c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-To-Do-Sachin-Nimshan/static/media/logo.5d5d9eef.svg"
+  }
+]);
